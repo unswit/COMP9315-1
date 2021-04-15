7f1496c79a5bbd942c6c6fca606153ce3776df3f
Harry <z5173587@student.unsw.edu.au>
2021-04-15 21:10:49 +1000

    update

--- ass2/ass2/x1.c
+++ ass2/ass2/x1.c
@@ -8,35 +8,9 @@
 
 int main(int argc, char **argv)
 {
-	Bits b = newBits(60);
-	printf("t=0: "); showBits(b); printf("\n");
-	setBit(b, 5);
-	printf("t=1: "); showBits(b); printf("\n");
-	setBit(b, 0);
-	setBit(b, 50);
-	setBit(b, 59);
-	printf("t=2: "); showBits(b); printf("\n");
-	if (bitIsSet(b,5)) printf("Bit 5 is set\n");
-	if (bitIsSet(b,10)) printf("Bit 10 is set\n");
-	setAllBits(b);
-	printf("t=3: "); showBits(b); printf("\n");
-	unsetBit(b, 40);
-	printf("t=4: "); showBits(b); printf("\n");
-	if (bitIsSet(b,20)) printf("Bit 20 is set\n");
-	if (bitIsSet(b,40)) printf("Bit 40 is set\n");
-	if (bitIsSet(b,50)) printf("Bit 50 is set\n");
-	setBit(b, 59);
-	unsetAllBits(b);
-	setBit(b, 10);
-	setBit(b, 15);
-	setBit(b, 1);
-	setBit(b, 5);
-	setBit(b, 59);
-	shiftBits(b, 5);
+	Bits b = newBits(13);
 	showBits(b);
-	printf("\n");
-	shiftBits(b, -6);
-	showBits(b);
-	printf("\n");
+	shiftBits(b, 1);
+	freeBits(b);
 	return 0;
 }
